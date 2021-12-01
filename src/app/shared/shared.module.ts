0a7696830140fa60c2bf6acea545641d1cccc401
ectuser <m.ectuser@gmail.com>
2021-12-02 01:16:48 +0700

    Removed unnecessary pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PercentPipe } from './pipes/percent.pipe';
 
 @NgModule({
-  declarations: [PercentPipe],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [ReactiveFormsModule],
 })
